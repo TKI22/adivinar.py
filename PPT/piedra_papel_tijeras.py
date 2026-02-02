3add478acad1f756b88b0b57fbd8a74a4f9da2f7
EvanCollazo <evancollazoo@gmail.com>
2026-02-02 10:22:55 -0300

    Hicimios un juego de Piedra, Papel o Tijera en Python

---
+++ PPT/piedra_papel_tijeras.py
@@ -0,0 +1,15 @@
+
+nombre1 = input("Jugador 1 ¿Cuál es tu nombre?: ")
+nombre2 = input("Jugador 2 ¿Cuál es tu nombre?: ")
+print()
+
+jugador1 = input("Hola jugador 1, ¿Qué eliges? ¿Piedra, Papel o Tijera?: ").lower
+jugador2 = input("Hola jugador 2, ¿Qué eliges? ¿Piedra, Papel o Tijera?: ").lower
+print()
+
+if jugador1 == jugador2:
+    print("¡Empate!")
+elif (jugador1 == "piedra" and jugador2 == "tijeras") or (jugador1 == "papel" and jugador2 == "piedra") or (jugador1 == "tijera" and jugador2 == "papel"):
+    print(f'"{nombre1}" Ganó!')
+else:
+    print(f'"{nombre2}" Ganó!')
